e7568ea8659499c965f28b4a06b418546aad5bcd
Gonzalo <zalo250@gmail.com>
2021-10-09 22:03:10 +0200

    add content for no game club yet

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
 
     useEffect(() => {
         const fetchUserClubs = async () => {
-            const { data, error } =
+            const {data, error} =
                 await supabase.from('user_clubs')
                     .select(`*, clubs(*)`)
                     .eq('user_id', supabase.auth.user().id)
@@ -19,14 +19,30 @@ export default function Home() {
         fetchUserClubs()
     }, [])
 
+    function NoClubBox() {
+        return (
+            <div className="pricing-header p-3 pb-md-4 mx-auto text-center">
+                <h1 className="display-4 fw-normal">No game club yet 😱</h1>
+                <p className="fs-5 text-muted">
+                    You are not part of a video game club yet? Look for the club that best identifies you,
+                    where your friends are or create one yourself!!
+                </p>
+                <div className="btn-group" role="group" aria-label="Basic outlined example">
+                    <Link href="./clubs/new">
+                        <button type="button" className="btn btn-outline-primary">Create</button>
+                    </Link>
+                    <Link href="./clubs">
+                        <button type="button" className="btn btn-outline-primary">Search</button>
+                    </Link>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div>
-            <Link href="./clubs/new">
-                Create Club
-            </Link>
-            <hr/>
             {
-                userClubs
+                userClubs && userClubs.length > 0
                     ? <div>
                         {userClubs.map((item) => {
                             return (
@@ -38,7 +54,7 @@ export default function Home() {
                             )
                         })}
                     </div>
-                    : <div>join to a club</div>
+                    : <NoClubBox/>
             }
         </div>
     )
