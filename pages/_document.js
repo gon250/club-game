c008a696a6e619b4d7d91b19e5e7bfefb06e846f
Gonzalo <zalo250@gmail.com>
2021-10-09 20:45:49 +0200

    add bootstrap css framework

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                        rel="stylesheet"
+                    />
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+                          rel="stylesheet"
+                          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+                          crossOrigin="anonymous"/>
+                </Head>
+                <body>
+                <Main/>
+                <NextScript/>
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
