8a41e3bf386099261f488cfe5ec6e303b5041e9c
Gonzalo <zalo250@gmail.com>
2021-10-09 18:03:34 +0200

    display your clubs

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import Link from 'next/link'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { supabase } from "../utils/supabaseClient";
 
 export default function Home() {
+    const [userClubs, setUserClubs] = useState(null)
 
     useEffect(() => {
+        const fetchUserClubs = async () => {
+            const { data, error } =
+                await supabase.from('user_clubs')
+                    .select(`*, clubs(*)`)
+                    .eq('user_id', supabase.auth.user().id)
+            if (data) {
+                console.log(data)
+                setUserClubs(data);
+            }
+        }
+        fetchUserClubs()
     }, [])
 
     return (
@@ -11,8 +24,24 @@ export default function Home() {
             <h1>Index</h1>
             <hr/>
             <Link href="./clubs/new">
-                <a>Create Club</a>
+                Create Club
             </Link>
+            <hr/>
+            {
+                userClubs
+                    ? <div>
+                        {userClubs.map((item) => {
+                            return (
+                                <div key={item.club_id}>
+                                    Club Name: {item.clubs.name}
+                                    <br/>
+                                    Club Description: {item.clubs.description}
+                                </div>
+                            )
+                        })}
+                    </div>
+                    : <div>join to a club</div>
+            }
         </div>
     )
 }
